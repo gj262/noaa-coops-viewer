45a60693a77b29a52fc9f7271f8610bd59875c9e
Gavin Jefferies <gavin@defend7.com>
2016-01-15 10:15:52 -0800

    remove redux-simple-router

--- src/app.js
+++ src/app.js
@@ -2,15 +2,12 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { syncReduxAndRouter } from 'redux-simple-router'
 import routes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 
 const history = createBrowserHistory()
-const store = configureStore(window.__INITIAL_STATE__)
-
-syncReduxAndRouter(history, store, (state) => state.router)
+const store = configureStore()
 
 // Render the React application to the DOM
 ReactDOM.render(
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import { routeReducer } from 'redux-simple-router'
 import coOps from './coOps'
 
 export default combineReducers({
-  coOps,
-  router: routeReducer
+  coOps
 })
